493c8d789340e6ebd0833234b4ddb3d006638278
Rebel028 <cheburashka028@gmail.com>
2017-09-04 20:07:45 +0300

    Добавлен SimModels.cs

---
+++ SimBankSite/Models/SimModels.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace SimBankSite.Models
+{
+    public class Sim
+    {
+        public string Id { get; set; }
+        public string TelNumber { get; set; }
+    }
+
+    public class SimContext : DbContext
+    {
+        public SimContext() : base("name=SimContext") { }
+
+        public DbSet<Sim> ActiveSimCards { get; set; }
+    }
+}
