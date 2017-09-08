232433195240ab7cbf7819be56d18761b4f247fe
Rebel028 <cheburashka028@gmail.com>
2017-09-08 18:21:53 +0300

    Еще больше сраных миграций

---
+++ SimBankSite/Migrations/201709081516349_decimaldrop.cs
@@ -0,0 +1,19 @@
+namespace SimBankSite.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class decimaldrop : DbMigration
+    {
+        public override void Up()
+        {
+            DropColumn("dbo.AspNetUsers", "Money");
+        }
+
+        public override void Down()
+        {
+            AddColumn("dbo.AspNetUsers", "Money", c => c.Decimal(nullable: false, precision: 18, scale: 2));
+        }
+    }
+}
+
