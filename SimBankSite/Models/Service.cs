57681093f22b51d6ebd70a9d6f6a633370b456d6
Rebel028 <cheburashka028@gmail.com>
2017-09-19 18:03:45 +0300

    верстка

--- SimBankSite/Models/Service.cs
+++ SimBankSite/Models/Service.cs
@@ -16,6 +16,7 @@ namespace SimBankSite.Models
         [Display(Name = "Цена")]
         public decimal Price { get; set; }
         [Display(Name = "Номер отправителя СМС")]
+        [DisplayFormat(ConvertEmptyStringToNull = false)]
         public string SenderNumber { get; set; }
     }
 }
